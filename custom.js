0f7e5cdb1d8502aca894c1e062e0611d601535bc
xDrews <43134259+xDrews@users.noreply.github.com>
2018-11-20 16:28:29 +0000

    amazon links changed to more accurated ones

---
+++ custom.js
@@ -0,0 +1,7 @@
+<script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js">
+</script>
+<script>
+	window.sr = ScrollReveal();
+	sr.reveal('.container-fluid');
+	sr.reveal('.ing');
+</script>
