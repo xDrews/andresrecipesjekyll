28a0fc72f3d485c1e76d2542371fabaa998c17d9
xDrews <43134259+xDrews@users.noreply.github.com>
2019-03-19 11:20:34 +0000

    Add files via upload

---
+++ analyticstracking.php
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-136535439-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-136535439-1');
+</script>
